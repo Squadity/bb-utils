4ce6675cd80bfb2cc1ea197a95d6fac7d2742e59
Alexandr Bolbat <alexandr@bolbat.net>
2015-01-26 15:32:44 +0200

    improved ToStringUtils with methods for maps

--- src/net/bolbat/utils/lang/ToStringUtils.java
+++ src/net/bolbat/utils/lang/ToStringUtils.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Utilities for <code>toString</code>.
@@ -26,6 +27,11 @@ public final class ToStringUtils {
 	 * Result elements delimiter.
 	 */
 	public static final String DELIMITER = ",";
+
+	/**
+	 * Result elements delimiter for map elements.
+	 */
+	public static final String MAP_VALUE_DELIMITER = "|";
 
 	/**
 	 * Result suffix if elements in collection more then allowed in result.
@@ -119,4 +125,49 @@ public final class ToStringUtils {
 		builder.append(LAST_CHAR);
 		return builder.toString();
 	}
+
+	/**
+	 * Create string representation for map with default elements limit.
+	 * 
+	 * @param map
+	 *            map, can be <code>null</code> or empty
+	 * @return {@link String}
+	 */
+	public static String toString(final Map<?, ?> map) {
+		return toString(map, DEFAULT_LIMIT);
+	}
+
+	/**
+	 * Create string representation for map.
+	 * 
+	 * @param map
+	 *            map, can be <code>null</code> or empty
+	 * @param maxLen
+	 *            elements limit used in representation
+	 * @return {@link String}
+	 */
+	public static String toString(final Map<?, ?> map, final int maxLen) {
+		if (map == null || map.isEmpty())
+			return EMPTY_RESULT;
+		if (maxLen < 1)
+			return EMPTY_MORE_RESULT;
+
+		final StringBuilder builder = new StringBuilder(FIRST_CHAR);
+		int i = 0;
+		for (final Iterator<?> iterator = map.keySet().iterator(); iterator.hasNext() && i < maxLen; i++) {
+			if (i > 0)
+				builder.append(DELIMITER);
+
+			final Object key = iterator.next();
+			final Object value = map.get(key);
+			builder.append(key).append(MAP_VALUE_DELIMITER).append(value instanceof Collection ? toString(Collection.class.cast(value), maxLen) : value);
+		}
+
+		if (map.size() > maxLen)
+			builder.append(DELIMITER).append(MORE_CHARS);
+
+		builder.append(LAST_CHAR);
+		return builder.toString();
+	}
+
 }
--- test/junit/net/bolbat/utils/lang/ToStringUtilsTest.java
+++ test/junit/net/bolbat/utils/lang/ToStringUtilsTest.java
@@ -2,6 +2,8 @@ package net.bolbat.utils.lang;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,13 +22,13 @@ public class ToStringUtilsTest {
 	public void complexArraysTest() {
 		final Object[] nullArray = null;
 		Assert.assertEquals("[]", ToStringUtils.toString(nullArray));
-		Assert.assertEquals("[1]", ToStringUtils.toString(new Integer[] {1}));
-		Assert.assertEquals("[1,2,3,4,5]", ToStringUtils.toString(new Integer[] {1, 2, 3, 4, 5}));
-		Assert.assertEquals("[1,2,3,4,5,6,7,8,9,10,...]", ToStringUtils.toString(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}));
+		Assert.assertEquals("[1]", ToStringUtils.toString(new Integer[] { 1 }));
+		Assert.assertEquals("[1,2,3,4,5]", ToStringUtils.toString(new Integer[] { 1, 2, 3, 4, 5 }));
+		Assert.assertEquals("[1,2,3,4,5,6,7,8,9,10,...]", ToStringUtils.toString(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }));
 		Assert.assertEquals("[]", ToStringUtils.toString(nullArray, 0));
-		Assert.assertEquals("[...]", ToStringUtils.toString(new Integer[] {1, 2, 3, 4, 5}, 0));
-		Assert.assertEquals("[1,2]", ToStringUtils.toString(new Integer[] {1, 2}, 5));
-		Assert.assertEquals("[1,2,3,4,5,...]", ToStringUtils.toString(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, 5));
+		Assert.assertEquals("[...]", ToStringUtils.toString(new Integer[] { 1, 2, 3, 4, 5 }, 0));
+		Assert.assertEquals("[1,2]", ToStringUtils.toString(new Integer[] { 1, 2 }, 5));
+		Assert.assertEquals("[1,2,3,4,5,...]", ToStringUtils.toString(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, 5));
 	}
 
 	/**
@@ -45,4 +47,37 @@ public class ToStringUtilsTest {
 		Assert.assertEquals("[1,2,3,4,5,...]", ToStringUtils.toString(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 5));
 	}
 
+	/**
+	 * Complex utility test with maps.
+	 */
+	@Test
+	public void complexMapsTest() {
+		final Map<Object, Object> nullMap = null;
+		Assert.assertEquals("[]", ToStringUtils.toString(nullMap));
+		Assert.assertEquals("[]", ToStringUtils.toString(nullMap, 0));
+
+		final Map<Object, Object> map = new HashMap<>();
+		map.put("key1", "value1");
+		Assert.assertEquals("[key1|value1]", ToStringUtils.toString(map));
+		Assert.assertEquals("[...]", ToStringUtils.toString(map, 0));
+
+		map.put("key2", "value2");
+		map.put("key3", "value3");
+		map.put("list1", Arrays.asList(1, 2, 3));
+		Assert.assertTrue(ToStringUtils.toString(map).contains("key1|value1"));
+		Assert.assertTrue(ToStringUtils.toString(map).contains("key2|value2"));
+		Assert.assertTrue(ToStringUtils.toString(map).contains("key3|value3"));
+		Assert.assertTrue(ToStringUtils.toString(map).contains("list1|[1,2,3]"));
+
+		map.put("list2", Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
+		Assert.assertTrue(ToStringUtils.toString(map).contains("list2|[1,2,3,4,5,6,7,8,9,10,...]"));
+		Assert.assertFalse(ToStringUtils.toString(map, 12).contains("list2|[1,2,3,4,5,6,7,8,9,10,...]"));
+
+		final Map<Object, Object> bigMap = new HashMap<>();
+		for (int i = 0; i < 15; i++)
+			bigMap.put("key" + i, "value" + i);
+
+		Assert.assertTrue(ToStringUtils.toString(bigMap).contains("...]"));
+		Assert.assertFalse(ToStringUtils.toString(bigMap, 15).contains("...]"));
+	}
 }
