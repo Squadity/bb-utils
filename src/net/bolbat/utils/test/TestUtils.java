731170eae195a7efe0974eaf948162cd228cd5a8
Alexandr Bolbat <alexandr@bolbat.net>
2016-01-13 16:51:04 +0200

    added some deprecation

--- src/net/bolbat/utils/test/TestUtils.java
+++ src/net/bolbat/utils/test/TestUtils.java
@@ -18,24 +18,28 @@ public final class TestUtils {
 
 	/**
 	 * Get path to temporary folder.<br>
-	 * Path always ends with path separator.
+	 * Path always ends with path separator.<br>
+	 * Deprecated, use {@code FSUtils.getTmpFolder(Class<?> clazz)} instead.
 	 * 
 	 * @param clazz
 	 *            class, used to construct path suffix from class name, optional
 	 * @return {@link String} path
 	 */
+	@Deprecated
 	public static String getTemporaryFolder(final Class<?> clazz) {
 		return FSUtils.getTmpFolder(clazz);
 	}
 
 	/**
 	 * Get path to temporary folder.<br>
-	 * Path always ends with path separator.
+	 * Path always ends with path separator.<br>
+	 * Deprecated, use {@code FSUtils.getTmpFolder(String... subPaths)} instead.
 	 * 
 	 * @param subPath
 	 *            sub path, optional
 	 * @return {@link String} path
 	 */
+	@Deprecated
 	public static String getTemporaryFolder(final String subPath) {
 		return FSUtils.getTmpFolder(subPath);
 	}
