19aa12ab8a4f113e3ded0b4691caf896e80626b0
h3llka <h3llka@gmail.com>
2017-01-25 13:06:55 +0200

    additionally junit test append with Generic ProxyHandlerSupport test case and proxy over proxy test case....

--- test/junit/net/bolbat/utils/reflect/proxy/ProxyUtilsTest.java
+++ test/junit/net/bolbat/utils/reflect/proxy/ProxyUtilsTest.java
@@ -3,6 +3,9 @@ package net.bolbat.utils.reflect.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,7 +18,7 @@ import net.bolbat.utils.reflect.ClassUtils;
 
 /**
  * {@link ProxyUtils} test.
- * 
+ *
  * @author Alexandr Bolbat
  */
 public class ProxyUtilsTest {
@@ -83,6 +86,92 @@ public class ProxyUtilsTest {
 		final Object target = support.getTarget(handler);
 		Assert.assertEquals(service, target);
 		Assert.assertSame(service, target);
+	}
+
+
+	/**
+	 * Generic proxy unwrap test case.. Uses  same Advised interface, but with {@link AdditionalAdvisedProxySupport} instead of
+	 * {@link AdvisedHandlerSupport}.
+	 */
+	@Test
+	public void genericProxySupportTest() {
+		final ManagedService service = new ManagedServiceImpl();
+		final Class<?>[] interfaces = ClassUtils.getAllInterfaces(ManagedServiceImpl.class);
+		List<Class<?>> interfacesList = new ArrayList<>(Arrays.asList(interfaces));
+		//appending Advised to interfaces list... ( as spring @ least does )....
+		interfacesList.add(Advised.class);
+		final InvocationHandler handler = new ManagedServiceHandler(service, interfaces);
+		final ManagedService proxy = CastUtils.cast(Proxy.newProxyInstance(service.getClass().getClassLoader(), interfacesList.toArray(new Class<?>[interfacesList.size()]), handler));
+
+		Assert.assertTrue(Proxy.isProxyClass(proxy.getClass()));
+		Assert.assertNotEquals(service, proxy);
+		Assert.assertNotSame(service, proxy);
+
+		final AdditionalAdvisedProxySupport support = new AdditionalAdvisedProxySupport();
+		ProxyUtils.registerProxyHandlerSupport(support);
+		Assert.assertEquals(Advised.class, support.getSupportedType());
+
+		final Object unwrapped = ProxyUtils.unwrapProxy(proxy);
+		Assert.assertSame(service, unwrapped);
+		Assert.assertFalse(Proxy.isProxyClass(unwrapped.getClass()));
+	}
+
+	/**
+	 * Current test case performs double proxy preparation and  further unwrap..
+	 */
+	@Test
+	public void proxyOverProxyTestCase() {
+		final ManagedService service = new ManagedServiceImpl();
+		final Class<?>[] interfaces = ClassUtils.getAllInterfaces(ManagedServiceImpl.class);
+		// Prepare  first  proxy .. based on AdvisedHandler .. where   AdvisedHandlerSupport used - as main unwrapped
+		final InvocationHandler firstProxyHandler = new ManagedServiceHandler(service, interfaces);
+		final ManagedService proxy = CastUtils.cast(Proxy.newProxyInstance(service.getClass().getClassLoader(), interfaces, firstProxyHandler));
+		//unwrapping
+		final Object unwrapped = ProxyUtils.unwrapProxy(proxy);
+		Assert.assertTrue(Proxy.isProxyClass(proxy.getClass()));
+		Assert.assertNotEquals(service, proxy);
+		Assert.assertNotSame(service, proxy);
+		Assert.assertFalse(Proxy.isProxyClass(unwrapped.getClass()));
+
+		// NOW lets create additional proxyUnder CurrentProxy....
+		final List<Class<?>> interfacesList = new ArrayList<>(Arrays.asList(interfaces));
+		// appending Advised  to the proxied interfaces....
+		interfacesList.add(Advised.class);
+		final Class<?>[] requiredProxyOverProxyInterfaces = interfacesList.toArray(new Class<?>[interfacesList.size()]);
+		final InvocationHandler secondProxyHandler = new ManagedServiceHandler(proxy, requiredProxyOverProxyInterfaces);
+
+		final ManagedService proxyOverProxy =
+				CastUtils.cast(Proxy.newProxyInstance(service.getClass().getClassLoader(), requiredProxyOverProxyInterfaces, secondProxyHandler));
+		//re-registering Additional ProxySupport<Advised> for test  purposes...
+		final AdditionalAdvisedProxySupport support = new AdditionalAdvisedProxySupport();
+		ProxyUtils.registerProxyHandlerSupport(support);
+		Assert.assertEquals(Advised.class, support.getSupportedType());
+
+
+		final Object secondTimeUnwrapped = ProxyUtils.unwrapProxy(proxyOverProxy);
+		Assert.assertSame(service, unwrapped);
+		Assert.assertSame(service, secondTimeUnwrapped);
+		Assert.assertSame(unwrapped, secondTimeUnwrapped);
+		Assert.assertFalse(Proxy.isProxyClass(secondTimeUnwrapped.getClass()));
+	}
+
+
+	/**
+	 * Additional ProxySupport for Advised. Required for generic testCase.
+	 */
+	private static class AdditionalAdvisedProxySupport implements ProxySupport<Advised> {
+		@Override
+		public Class<?> getSupportedType() {
+			return Advised.class;
+		}
+
+		@Override
+		public Object getTarget(Advised source) {
+			if (Proxy.isProxyClass(source.getClass()))
+				return Advised.class.cast(Proxy.getInvocationHandler(source)).getProxiedTarget();
+			return source.getProxiedTarget();
+		}
+
 	}
 
 }
